55b9b5284ae0e78c590d0e0ee97eeb590b048714
22002 <2200214670@qq.com>
2025-08-09 17:31:09 +0800

    Button coding

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,36 @@
+import Button, { ButtonSize, ButtonType } from '@/components/Button';
+
 function App() {
   return (
     <>
-      
+      <div
+        style={{
+          display: 'flex',
+          gap: 10,
+          marginTop: 10,
+          marginLeft: 10,
+        }}
+      >
+        <Button>Default Button</Button>
+        <Button btnType={ButtonType.Primary}>Primary Button</Button>
+        <Button btnType={ButtonType.Danger}>Danger Button</Button>
+        <Button btnType={ButtonType.Link} href="xx">
+          Link Button
+        </Button>
+        <Button disabled>Disabled Button</Button>
+      </div>
+      <div
+        style={{
+          marginTop: 10,
+          marginLeft: 10,
+        }}
+      >
+        <Button>Normal Button</Button>
+        <Button size={ButtonSize.Small}>Small Button</Button>
+        <Button size={ButtonSize.Large}>Large Button</Button>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import { resolve } from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+      '@components': resolve(__dirname, 'src/components'),
+    },
+  },
+});
