cdceacc73b599216bc2a48464bf0c1fb4a0dd6b8
yty010706 <yintianyang283@gmail.com>
2025-08-25 12:01:04 +0800

    Test CI

--- jest.config.ts
+++ jest.config.ts
@@ -1,28 +1,15 @@
 import type { Config } from '@jest/types';
-import fs from 'fs';
-import path from 'path';
-
-// 检查 setupTests.ts 是否存在，如果不存在则使用 .js 版本
-const setupTestsPath = fs.existsSync(
-  path.resolve(__dirname, 'src/setupTests.ts')
-)
-  ? '<rootDir>/src/setupTests.ts'
-  : '<rootDir>/src/setupTests.js';
 
 const config: Config.InitialOptions = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
-  setupFilesAfterEnv: [setupTestsPath],
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
     '^@components/(.*)$': '<rootDir>/src/components/$1',
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
   },
-  testPathIgnorePatterns: [
-    '<rootDir>/node_modules/',
-    '<rootDir>/dist/',
-    '<rootDir>/src/**/*.stories.tsx',
-  ],
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/dist/'],
   collectCoverageFrom: [
     'src/components/**/*.{ts,tsx}',
     '!src/**/*.stories.tsx',
